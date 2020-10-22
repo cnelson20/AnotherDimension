46ac6205317f26c571eea1199ba307533c0adf3c
cnelson20 <cjnelson2004@gmail.com>
2020-10-22 13:04:55 -0400

    sum now works!

---
+++ ArrayOps.java
@@ -0,0 +1,9 @@
+public class ArrayOps {
+  public static int sum(int[] arr) {
+    int s = 0;
+    for (int i = 0; i < arr.length; i++) {
+      s += arr[i];
+    }
+    return s;
+  }
+}
---
+++ Tester.java
@@ -0,0 +1,6 @@
+public class Tester {
+  public static void main(String[] args) {
+    int[] test = {1,2,3,4};
+    System.out.println(ArrayOps.sum(test));
+  }
+}
