e542bb9b8698cc2d7c4c8d9e1908d9677e71a283
cnelson20 <cjnelson2004@gmail.com>
2020-10-22 13:14:35 -0400

    sumRows works

--- Tester.java
+++ Tester.java
@@ -1,7 +1,11 @@
+import java.util.Arrays;
+
 public class Tester {
   public static void main(String[] args) {
     int[] test = {1,2,3,4};
+    int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
     System.out.println("Sum: " + ArrayOps.sum(test));
     System.out.println("Largest Value: " + ArrayOps.largest(test));
+    System.out.println("Sum of rows: " + Arrays.toString(ArrayOps.sumRows(matrix)));
   }
 }
