635e64b4520ca929830ade81b8e030e9f92b072b
cnelson20 <cjnelson2004@gmail.com>
2020-10-22 13:18:43 -0400

    sum() works with matrices now

--- ArrayOps.java
+++ ArrayOps.java
@@ -23,4 +23,8 @@ public class ArrayOps {
     }
     return sums;
   }
+
+  public static int sum(int[][] matrix) {
+    return sum(sumRows(matrix));
+  }
 }
--- Tester.java
+++ Tester.java
@@ -7,5 +7,6 @@ public class Tester {
     System.out.println("Sum: " + ArrayOps.sum(test));
     System.out.println("Largest Value: " + ArrayOps.largest(test));
     System.out.println("Sum of rows: " + Arrays.toString(ArrayOps.sumRows(matrix)));
+    System.out.println("Sum of everything: " + ArrayOps.sum(matrix));
   }
 }
